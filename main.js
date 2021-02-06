2b1c44ecf07a6ecb6bbd35be653e3eb7dbb202f6
tatiana perez <tjperez@ncsu.edu>
2021-02-06 14:40:35 -0500

    can successfully create new note

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const notesUrl = "http://localhost:3000/notes/"
+let form = document.querySelector("form")
+const button = document.querySelector(".submit-note")
+
+// window.addEventListener('click', e => {
+//     e.preventDefault();
+// })
+
+form.addEventListener('submit', e => {
+    postNotes();
+})
+
 
 
 function doNotes () {
@@ -7,14 +18,13 @@ function doNotes () {
         return response.json()
       })
       .then(function (data) {
-        console.log(data)
         for (let note of data) {
-          console.log(note)
           displayNotes(note)
         }
+        return data
       })
+     
   }
-
 
 
   function displayNotes (noteObj) {
@@ -24,35 +34,49 @@ function doNotes () {
     itemEl.classList.add("note-object")
     displayNoteText(itemEl, noteObj)
     noteList.appendChild(itemEl)
-    //clearInputs()
+    clearInputs()
   }
   
   function displayNoteText (noteListItem, noteObj) {
     noteListItem.innerHTML = `${noteObj.title}`
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function clearInputs() {
+  const inputs = document.querySelectorAll('input')
+  for (let field of inputs) {
+    field.value = ''
+  }
+}
+  
+// function postNotes() {
+//     return fetch(notesUrl).then(r => r.json()).then(data => console.log(data))
+// }
 
 function postNotes() {
-fetch(notesUrl, {
-  method: 'POST', 
-  headers: {"Content-Type": "application/json"}, 
-  body: JSON.stringify({"title": "Hi", "body": "COOL"})
-})
-.then(r => r.json())
-.then(
-  // whatever you need to do next
-)}
+    let titleNote = document.querySelector(".note-title").value
+    let bodyNote = document.querySelector(".note-body").value
+    if (titleNote.length > 1 || bodyNote.length > 1) {
+        fetch(notesUrl, {
+        method: 'POST', 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({
+        title: titleNote,
+        body: bodyNote
+        })}).then(
+            r => {
+                console.log(r)
+                r.json()
+            }
+        ).then(
+            data => {
+                console.log(data)
+            }
+        ).catch(
+            e => {console.log(e)}
+        );
+    }
+    titleNote.value = "";
+    bodyNote.value = "";
+}
 
 doNotes()
